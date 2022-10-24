acb98836452c8fe18b1443d53a51b74c41fddb0c
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-10-24 14:12:32 +0200

    current Version run

--- acasa/acasa/app.py
+++ acasa/acasa/app.py
@@ -8,9 +8,15 @@ from invoice import listItems, printOrder
 
 def main():
     menu_dict = loadData("acasa.json")
-    order = [("10", 2), ("11", 1)]
-    price_total, printable_list = listItems(order, menu_dict)
-    printOrder(price_total, printable_list)
+    order = [("10", 2), ("11", 1)]  # TODO whileloop for Order and Quit
+    try:
+        price_total, printable_list = listItems(order, menu_dict)
+    except KeyError:
+        pass
+        # TODO fehlenden Menüeintrag nacheintragen?
+        print("Menu überprüfen (acasa.json)")
+    else:
+        printOrder(price_total, printable_list)
 
 
 if __name__ == "__main__":
