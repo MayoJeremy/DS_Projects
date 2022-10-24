35651d4524e0768b953fbd2a74d3883371fbf805
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-10-24 10:22:06 +0200

    Adding Menuentries

--- acasa/acasa/menu.py
+++ acasa/acasa/menu.py
@@ -6,8 +6,25 @@ BASE_DIR = Path(__file__).parent.parent
 DATA_DIR = BASE_DIR / "data"
 
 
-def addEntry(menu_dict: dict, item_name: str, item_price: float) -> dict:
-    pass
+def addEntry(
+    menu_dict: dict,
+    item_name: str,
+    item_price: float,
+    item_category: str,
+    item_menu_id: str,
+) -> dict:
+    try:
+        menu_dict[item_category].update(
+            {
+                item_menu_id: {
+                    "Item_ID": item_name,
+                    "Price": float(item_price),
+                }
+            },
+        )
+    except KeyError:
+        menu_dict[item_category] = {}
+        addEntry(menu_dict, item_name, item_price, item_category, item_menu_id)
 
 
 def loadData(file_path: str) -> dict:
@@ -26,7 +43,15 @@ def saveData(file_path: str, menu_dict: dict) -> None:
 
 
 def main():
-    loadData(DATA_DIR / "test.json")
+    test_dict = loadData(DATA_DIR / "test.json")
+    addEntry(
+        test_dict,
+        item_category="Pizza",
+        item_price=1.25,
+        item_name="Pizza Thunfisch",
+        item_menu_id="101",
+    )
+    saveData(DATA_DIR / "test.json", test_dict)
 
 
 if __name__ == "__main__":
