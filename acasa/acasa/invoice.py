2af391291ac551dfbc4a73b128e489a547b5c819
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-10-24 11:50:42 +0200

    calculating Sum of Order

--- acasa/acasa/invoice.py
+++ acasa/acasa/invoice.py
@@ -3,8 +3,17 @@
 from menu import loadData
 
 
+def calcTotal(order: list, menu_dict: dict):
+    total_cost = 0.0
+    for sorder in order:
+        total_cost += menu_dict[str(sorder)]["Price"]
+    return total_cost
+
+
 def main():
-    pass
+    menu_dict = loadData("acasa.json")
+    order = [10, 11, 10]
+    print(calcTotal(order, menu_dict))
 
 
 if __name__ == "__main__":
