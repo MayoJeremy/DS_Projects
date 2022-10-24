de9e481ea3f1b45085e908d5bdc768c8b8e7b985
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-10-24 09:20:49 +0200

    Base

---
+++ acasa/acasa/app.py
@@ -0,0 +1,11 @@
+"""
+Hauptprogramm
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ acasa/acasa/menu.py
@@ -0,0 +1,22 @@
+"""Menu-file"""
+from pathlib import Path
+import pickle
+
+BASE_DIR = Path(__file__).parent.parent
+DATA_DIR = BASE_DIR / "data"
+
+
+def addEntry(menu_dict: dict, item_name: str, item_price: float) -> dict:
+    pass
+
+
+def loadData():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
