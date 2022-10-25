ae526291df1559f4458c5d144ca6a2430e678627
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-10-25 14:51:17 +0200

    Menucontrol

--- 01_acasa/acasa/app.py
+++ 01_acasa/acasa/app.py
@@ -1,6 +1,7 @@
 """
 Hauptprogramm
 """
+from cgitb import reset
 import os
 import sqlite3
 from pathlib import Path
@@ -12,13 +13,37 @@ os.chdir(Path(__file__).parent)
 
 conn = sqlite3.connect("../data/store.db")
 cursor = conn.cursor()
+ADMIN = False
+WELCOME_MSG = "Willkommen bei Acasa!"
 
 
 def get_menu():
     sql = "SELECT  * FROM Menu;"
     cursor.execute(sql)
-    results = cursor.fetchall()
-    return results
+    return cursor.fetchall()
+
+
+def get_menu_indexes():
+    sql = "SELECT Dish_ID From Menu;"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    ite_result = []
+    for r in result:
+        ite_result.append(r[0])
+    return ite_result
+
+
+def string_menu(menu: str):
+    menu_string = ""
+    for _, dish in enumerate(menu):
+        if not menu_string:
+            menu_string += f"{dish[2]}\n" + 40 * "_" + "\n"
+            menu_string += "{}. {}\t|\t{}€\n".format(dish[0], dish[1], dish[3])
+            continue
+        if menu[_][2] != menu[_ - 1][2]:
+            menu_string += f"\n{dish[2]}\n" + 40 * "_" + "\n"
+        menu_string += "{}. {}\t|\t{}€\n".format(dish[0], dish[1], dish[3])
+    return menu_string
 
 
 def insert_dish(data):
@@ -46,10 +71,15 @@ def get_order():
     counter = Counter()
     order_list = []
     order = []
+    accepted_items = get_menu_indexes()
+
     while 1:
         user_in = int(input("0 to quit | choice >> "))
         if not user_in:
             break
+        if user_in not in accepted_items:
+            print("Invalid Item selected. Try again")
+            continue
         counter[user_in] += 1
         order_list.append(user_in)
     for order_id, order_name in counter.items():
@@ -77,11 +107,12 @@ def make_receipt(orders: list):
             )
         )
         receipt_total += dish_total
+
     return receipt, receipt_total
 
 
 def string_receipt(receipt_items: list, receipt_total: float):
-    message = ""
+    message = "Quittung\n" + 40 * "_" + "\n"
     for row in receipt_items:
         message += row + "\n"
     message += 40 * "*" + "\n"
@@ -91,18 +122,26 @@ def string_receipt(receipt_items: list, receipt_total: float):
 
 def save_receipt(receipt: str):
     with open("../data/receipt.txt", "w", encoding="UTF-8") as file:
-        file.write(receipt)
+        file.write(receipt.strip())
 
 
 def main():
-    # file = "../data/menu.json"
-    # import_json_db(file)
-    # pprint.pprint(get_menu())
+    if ADMIN:
+        file = "../data/menu.json"
+        import_json_db(file)
+
+    print(WELCOME_MSG)
+    menu = get_menu()
+    print(string_menu(menu))
+
+    print("Ihre Bestellung")
     order = get_order()
+
     receipt = make_receipt(order)
     receipt_string = string_receipt(*receipt)
     print(receipt_string)
     save_receipt(receipt_string)
+
     conn.close()
 
 
