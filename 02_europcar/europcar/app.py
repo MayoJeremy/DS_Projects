181aa63658c6f42c34a94d77348ee675282879ff
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-10-27 10:01:41 +0200

    Retrieve list of all brands in db

--- 02_europcar/europcar/app.py
+++ 02_europcar/europcar/app.py
@@ -8,25 +8,34 @@ cursor = conn.cursor()
 GREETING_MSG = "Welcome to Europcar!"
 
 
-def get_catalog_string(brand: str) -> str:
-    car_catalog = [brand + ": "]
+def get_catalog_string(brand: str, availability: int = 1) -> str:
+    car_catalog = [brand.capitalize() + ": "]
     sql = "SELECT CarID, Model.Title, Color.Title \
 FROM Car \
 LEFT JOIN Model USING(ModelID) \
 LEFT JOIN Color USING(ColorID) \
 LEFT JOIN Brand USING(BrandID) \
-WHERE StatusID = 1 AND Brand.Title = ?\
+WHERE StatusID = ? AND Brand.Title = ?\
 ;"
-    cursor.execute(sql, [brand])
+    cursor.execute(sql, [availability, brand])
     for entry in cursor.fetchall():
         car_catalog.append("{}. | {} | {}".format(*entry))
     return "\n".join(car_catalog)
 
 
+def get_brands():
+    brand_names = []
+    sql = "SELECT title FROM Brand"
+    cursor.execute(sql)
+    for brand in cursor.fetchall():
+        brand_names.append(brand[0])
+    return brand_names
+
+
 def main():
     print(GREETING_MSG)
-    car_catalog = get_catalog_string("bmw")
-    print(car_catalog)
+    for _ in get_brands():
+        print("\n", get_catalog_string(_))
 
 
 if __name__ == "__main__":
