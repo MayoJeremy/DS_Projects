4370e743db1535fe958c1f068ad57759cb185474
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-03 13:51:59 +0100

    Import as Dict

---
+++ 03_acasa_oop/acasa/management/json_import.py
@@ -0,0 +1,31 @@
+import json
+import os
+from pathlib import Path
+
+
+os.chdir(Path(__file__).parent.parent.parent)
+
+
+class JsonImporter:
+    def __init__(self, file: str) -> None:
+        """
+
+        Args:
+            file (str): Path + Filename to import
+        """
+        self.file = file  # "./data/menu.json"
+
+    def get_content(self):
+        """loads Jsonfile into instance based variable 'menu'"""
+        with open(self.file, mode="r", encoding="UTF-8") as file_in:
+            self.menu = json.load(file_in)
+
+
+def main():
+    js1 = JsonImporter()
+    js1.get_content()
+    print(js1.menu)
+
+
+if __name__ == "__main__":
+    main()
