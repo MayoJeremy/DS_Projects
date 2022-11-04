c108ae0714acfa94c868c895a012d9ff89cc4b23
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-04 13:25:17 +0100

    getting Order and saving to DB

--- 03_acasa_oop/app.py
+++ 03_acasa_oop/app.py
@@ -3,7 +3,7 @@ Hauptprogramm Neuversuch
 """
 from acasa.restaurant.menu import Menu
 from acasa.restaurant.customer import Customer
-from acasa.restaurant.menu import Menu
+from acasa.restaurant.order import Order
 
 
 WELCOME_MSG = "Welcome to Acasa Restaurant"
@@ -18,10 +18,30 @@ def get_customer_info():
     return first_name, last_name, tel
 
 
+def get_customer_order_list(menu: Menu):
+    order_list = []
+    while True:
+        user_in = input("(0) to end Order.\nYour Order-nr >> ")
+
+        if user_in.isdigit():
+            user_in = int(user_in)
+        else:
+            print("Please put in a Number.")
+            continue
+        user_order = menu.menu_dishes.get(user_in)
+        if user_in == 0:
+            return order_list
+        elif not user_order:
+            print("Please put a valid Order Number in.")
+            continue
+        order_list.append(user_order)
+
+
 def main():
-    menu = Menu("acasa.db")
+    menu = Menu(DB_NAME)
     menu.import_menu_items_from_db()
     menu.initialize_menu_items()
+    # print(menu.menu)
 
     print(WELCOME_MSG)
     print()
@@ -29,11 +49,12 @@ def main():
     customer = Customer(*get_customer_info(), DB_NAME)
     customer.input_customer()
 
-    print(customer)
-    menu.print_menu()
 
-    while True:
-        break
+    menu.print_menu()
+    cust_order_list = get_customer_order_list(menu)
+    cust_order = Order(customer, cust_order_list, DB_NAME)
+    cust_order.process_dish_orders()
+    cust_order.print_receipt()
 
 
 if __name__ == "__main__":
