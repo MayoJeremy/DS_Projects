29c78f4423d75eabc72cd4e9eb226bf9dadee7c8
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-04 11:52:56 +0100

    generalize Dish

--- 03_acasa_oop/acasa/restaurant/dish.py
+++ 03_acasa_oop/acasa/restaurant/dish.py
@@ -1,23 +1,9 @@
 class Dish:
-    def __init__(self, dish_id, title, price) -> None:
+    def __init__(self, dish_id, title, category, price) -> None:
         self.dish_id = dish_id
         self.title = title
+        self.category = category
         self.price = price
 
     def __repr__(self) -> str:
         return f"[ {self.dish_id} ]: {self.title}\t- {self.price}"
-
-
-class Pizza(Dish):
-    def __init__(self, dish_id, title, price) -> None:
-        super().__init__(dish_id, title, price)
-
-
-class Auflauf(Dish):
-    def __init__(self, dish_id, title, price) -> None:
-        super().__init__(dish_id, title, price)
-
-
-class Salat(Dish):
-    def __init__(self, dish_id, title, price) -> None:
-        super().__init__(dish_id, title, price)
--- 03_acasa_oop/acasa/restaurant/menu.py
+++ 03_acasa_oop/acasa/restaurant/menu.py
@@ -1,16 +1,14 @@
 import os
 import sqlite3
 from pathlib import Path
-from acasa.restaurant.dish import Pizza, Auflauf, Salat
+from acasa.restaurant.dish import Dish
 
 os.chdir(Path(__file__).parent.parent.parent)
 
 
 class Menu:
     def __init__(self, db_name) -> None:
-        self.list_pizza = []
-        self.list_auflauf = []
-        self.list_salat = []
+        self.menu_dishes = []
         self.conn = sqlite3.connect("./data/" + db_name)
         self.cursor = self.conn.cursor()
 
@@ -21,41 +19,21 @@ class Menu:
 
     def initialize_menu_items(self):
         for dish in self.menu:
-            if dish[2] == "Pizza":
-                self.list_pizza.append(
-                    Pizza(
-                        dish[0],
-                        dish[1],
-                        dish[3],
-                    )
-                )
-            if dish[2] == "Auflauf":
-                self.list_auflauf.append(
-                    Auflauf(
-                        dish[0],
-                        dish[1],
-                        dish[3],
-                    )
-                )
-            if dish[2] == "Salat":
-                self.list_salat.append(
-                    Salat(
-                        dish[0],
-                        dish[1],
-                        dish[3],
-                    )
-                )
+            self.menu_dishes.append(Dish(*dish))
 
     def print_menu(self):
         print("Pizza:\n")
-        for pizza in self.list_pizza:
-            print(pizza)
+        for dish in self.menu_dishes:
+            if dish.category == "Pizza":
+                print(dish)
         print("\nAuflauf:\n")
-        for auflauf in self.list_auflauf:
-            print(auflauf)
+        for dish in self.menu_dishes:
+            if dish.category == "Auflauf":
+                print(dish)
         print("\nSalat:\n")
-        for salat in self.list_salat:
-            print(salat)
+        for dish in self.menu_dishes:
+            if dish.category == "Salat":
+                print(dish)
 
 
 def main():
