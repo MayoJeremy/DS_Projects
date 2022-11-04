5ed44792bb6bd73d34c548c87d42fed2250de97d
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-04 14:28:36 +0100

    moved responsibilty to SQL Handler

--- 03_acasa_oop/acasa/restaurant/order.py
+++ 03_acasa_oop/acasa/restaurant/order.py
@@ -1,32 +1,8 @@
-import os
-import sqlite3
-from pathlib import Path
-from acasa.restaurant.customer import Customer
-from acasa.restaurant.dish import Dish
-
-os.chdir(Path(__file__).parent.parent.parent)
-
-
 class Order:
-    def __init__(self, customer, dish_order_list, db_name) -> None:
+    def __init__(self, customer, dish_order_list) -> None:
         self.customer = customer
         self.dish_order_list = dish_order_list
         self.total = 0
-
-        self.conn = sqlite3.connect("./data/" + db_name)
-        self.cursor = self.conn.cursor()
-
-    def process_dish_orders(self):
-        for dish_order in self.dish_order_list:
-            self.input_order()
-
-    def input_order(self):
-        sql = "INSERT INTO 'Order' (CustomerID, DishID) VALUES (?,?);"
-        data = []
-        for dish_order in self.dish_order_list:
-            data.append((self.customer.customer_id, dish_order.dish_id))
-        self.cursor.executemany(sql, data)
-        self.conn.commit()
 
     def print_receipt(self):
         print(self.customer)
@@ -34,16 +10,3 @@ class Order:
             print(dish_order)
             self.total += dish_order.price
         print(f"Total = {self.total}")
-
-
-def main():
-    # test_Customer = Customer("Test", "Mest", "+49151515151", "acasa.db", 1)
-    # test_Customer.input_customer()
-    # test_order = Order(test_Customer, [(Pizza(100, "Pizza Margeritta", 5))], "acasa.db")
-    # test_order.print_receipt()
-    # test_order.input_order()
-    pass
-
-
-if __name__ == "__main__":
-    main()
