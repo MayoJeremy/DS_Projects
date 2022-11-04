b558b41eb31735422910252d242cd7417932168b
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-04 15:09:50 +0100

    use SQLhandler for Menu retrieving

--- 03_acasa_oop/acasa/restaurant/menu.py
+++ 03_acasa_oop/acasa/restaurant/menu.py
@@ -1,21 +1,10 @@
-import os
-import sqlite3
-from pathlib import Path
 from acasa.restaurant.dish import Dish
-
-os.chdir(Path(__file__).parent.parent.parent)
 
 
 class Menu:
-    def __init__(self, db_name) -> None:
+    def __init__(self, menu) -> None:
         self.menu_dishes = {}
-        self.conn = sqlite3.connect("./data/" + db_name)
-        self.cursor = self.conn.cursor()
-
-    def import_menu_items_from_db(self):
-        sql = "SELECT * FROM Menu;"
-        self.cursor.execute(sql)
-        self.menu = self.cursor.fetchall()
+        self.menu = menu
 
     def initialize_menu_items(self):
         for dish in self.menu:
@@ -34,14 +23,3 @@ class Menu:
         for dish in self.menu_dishes.values():
             if dish.category == "Salat":
                 print(dish)
-
-
-def main():
-    testmenu = Menu("acasa.db")
-    testmenu.import_menu_items_from_db()
-    testmenu.initialize_menu_items()
-    testmenu.print_menu()
-
-
-if __name__ == "__main__":
-    main()
