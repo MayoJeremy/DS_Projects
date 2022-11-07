0ae68ac48cdf5d91ebb88fa04eeaff832d4b6dea
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-07 14:06:42 +0100

    secondary url creation

--- 04_url_shortener/shortener/url.py
+++ 04_url_shortener/shortener/url.py
@@ -7,3 +7,12 @@ class Url:
         self.original_url = original_url
         self.short_url = short_url
         self.user_id = user_id
+
+    def __repr__(self) -> str:
+        return f"https://{self.domain_name}/{self.short_url}"
+
+    @classmethod
+    def create_new_url_via_input(cls, short_url, user_id):
+        original_url = input("Url to shorten >> ").split("//")[1]
+        domain_name = original_url.split("/")[0]
+        return Url(domain_name, original_url, short_url, user_id)
