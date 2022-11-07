f6e1591cea0e33eabd0cbef11abfb39b4b6a3c64
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-07 11:36:30 +0100

    Url Baseclass

---
+++ 04_url_shortener/shortener/url.py
@@ -0,0 +1,7 @@
+class Url:
+    def __init__(self, domain_name, original_url, short_url, user_id, url_id=0) -> None:
+        self.url_id = url_id
+        self.domain_name = domain_name
+        self.original_url = original_url
+        self.short_url = short_url
+        self.user_id = user_id
