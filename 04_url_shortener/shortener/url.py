596ace9b2226def58e23b226d9addde85f8d0e7e
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-07 11:37:58 +0100

    base_url variable

--- 04_url_shortener/shortener/url.py
+++ 04_url_shortener/shortener/url.py
@@ -1,4 +1,6 @@
 class Url:
+    base_url = "https://wbs.com"
+
     def __init__(self, domain_name, original_url, short_url, user_id, url_id=0) -> None:
         self.url_id = url_id
         self.domain_name = domain_name
