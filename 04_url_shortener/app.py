256a02c16627d732f5124dd58e7ff1035599a67d
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-08 14:34:19 +0100

    Cleanup url display mode

--- 04_url_shortener/app.py
+++ 04_url_shortener/app.py
@@ -38,6 +38,12 @@ def url_creation_mode(user: User, urls: list) -> None:
     new_url.save_to_db()
 
 
+def url_display_mode(user: User, urls: list) -> None:
+    for url in urls:
+        if url.user_id == user.user_id:
+            print(f"{url.domain_name} | {url.short_url} | {user.user_name}")
+
+
 def main():
     user = User.user_login()
     mode = get_mode()
@@ -47,10 +53,7 @@ def main():
     if mode == 1:
         url_creation_mode(user, urls)
     elif mode == 2:
-        for url in urls:
-            if url.user_id == user.user_id:
-                print(f"{url.domain_name} | {url.short_url} | {user.user_name}")
-
+        url_display_mode(user, urls)
     elif mode == 3:
         short_url = input("Shorturl >> ")
         for url in urls:
