a6cc176149392c95a31e3377d476ed6dd8e90b66
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-14 13:29:37 +0100

    Config

--- 05_serialletter/management/config.py
+++ 05_serialletter/management/config.py
@@ -11,3 +11,12 @@ with open("./data/config.json", mode="r", encoding="UTF-8") as file:
 
 
 DB_BASE = declarative_base()
+
+# Database credentials
+DB_HOST = config_dict.get("db_host")
+DB_USER = config_dict.get("db_user")
+DB_PASSWORD = config_dict.get("db_password")
+DB_NAME = config_dict.get("db_database")
+
+# Database connectionstring for sqlalchemy
+DB_CONNECTION_STRING = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
