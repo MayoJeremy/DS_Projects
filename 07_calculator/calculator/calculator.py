4a0b04feefcfea2c1ffa9934e5aa91b73997caf4
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-17 13:50:07 +0100

    change input entry to label

--- 07_calculator/calculator/calculator.py
+++ 07_calculator/calculator/calculator.py
@@ -5,13 +5,14 @@ import tkinter as tk
 class Calculator:
     def __init__(self, window) -> None:
         self.window = window
+        self.input_string = tk.StringVar()
         self.setup_gui()
 
     def setup_gui(self):
         self.window.title(cfg.APP_TITLE)
         self.window.geometry(f"{cfg.APP_WIDTH}x{cfg.APP_HEIGHT}")
         self.result_label()
-        self.input_entry()
+        self.input_label()
         self.create_numpad()
         self.create_operand_selection()
 
@@ -19,15 +20,15 @@ class Calculator:
         self.label_result = tk.Label(self.window, text="Result label")
         self.label_result.grid(row=0, column=0, columnspan=3)
 
-    def input_entry(self):
-        self.entry_input = tk.Entry(self.window, text="Input label")
-        self.entry_input.grid(row=1, column=0, columnspan=3)
+    def input_label(self):
+        self.label_input = tk.Label(self.window, textvariable=self.input_string)
+        self.label_input.grid(row=1, column=0, columnspan=3)
 
     def add_sym_to_input(self, symbol_to_add):
-        self.entry_input.insert(tk.END, symbol_to_add)
+        self.input_string.set(self.input_string.get() + symbol_to_add)
 
     def calculate_and_display_result(self):
-        result = eval(self.entry_input.get())
+        result = eval(self.input_string.get())  # careful, can insert pythoncode
         self.label_result.config(text=result)
 
     def create_numpad(self):
