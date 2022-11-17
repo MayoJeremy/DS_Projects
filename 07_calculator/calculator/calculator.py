f88763cde867870688a76956bf23c8302e16b805
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-17 14:54:07 +0100

    save result as variable string

--- 07_calculator/calculator/calculator.py
+++ 07_calculator/calculator/calculator.py
@@ -6,11 +6,13 @@ class Calculator:
     def __init__(self, window) -> None:
         self.window = window
         self.input_string = tk.StringVar()
+        self.result_string = tk.StringVar()
         self.setup_gui()
 
     def setup_gui(self):
         self.window.title(cfg.APP_TITLE)
         self.window.geometry(f"{cfg.APP_WIDTH}x{cfg.APP_HEIGHT}")
+        self.window.config(bg="white")
         self.result_label()
         self.input_label()
         self.create_numpad()
@@ -39,11 +41,22 @@ class Calculator:
         self.window.bind("<Return>", self.calculate_and_display_result)
 
     def result_label(self):
-        self.label_result = tk.Label(self.window, text="Result label")
+        self.label_result = tk.Label(
+            self.window,
+            width=cfg.LABEL_WIDTH,
+            height=cfg.LABEL_HEIGHT,
+            bg="grey",
+            textvariable=self.result_string,
+        )
         self.label_result.grid(row=0, column=0, columnspan=3)
 
     def input_label(self):
-        self.label_input = tk.Label(self.window, textvariable=self.input_string)
+        self.label_input = tk.Label(
+            self.window,
+            width=cfg.LABEL_WIDTH,
+            height=cfg.LABEL_HEIGHT,
+            textvariable=self.input_string,
+        )
         self.label_input.grid(row=1, column=0, columnspan=3)
 
     def add_sym_to_input(self, symbol_to_add):
@@ -51,7 +64,8 @@ class Calculator:
 
     def calculate_and_display_result(self, event=None):
         result = eval(self.input_string.get())  # careful, can insert pythoncode
-        self.label_result.config(text=result)
+        self.result_string.set(result)
+
 
     def clear_input_label(self, event=None):
         self.input_string.set("")
@@ -61,60 +75,70 @@ class Calculator:
             self.window,
             text="1",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("1"),
         ).grid(row=2, column=0)
         tk.Button(
             self.window,
             text="2",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("2"),
         ).grid(row=2, column=1)
         tk.Button(
             self.window,
             text="3",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("3"),
         ).grid(row=2, column=2)
         tk.Button(
             self.window,
             text="4",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("4"),
         ).grid(row=3, column=0)
         tk.Button(
             self.window,
             text="5",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("5"),
         ).grid(row=3, column=1)
         tk.Button(
             self.window,
             text="6",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("6"),
         ).grid(row=3, column=2)
         tk.Button(
             self.window,
             text="7",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("7"),
         ).grid(row=4, column=0)
         tk.Button(
             self.window,
             text="8",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("8"),
         ).grid(row=4, column=1)
         tk.Button(
             self.window,
             text="9",
             width=cfg.NUM_WIDTH,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("9"),
         ).grid(row=4, column=2)
         tk.Button(
             self.window,
             text="0",
             width=cfg.NUM_WIDTH * 3,
+            height=cfg.NUM_HEIGHT,
             command=lambda: self.add_sym_to_input("0"),
         ).grid(row=5, column=0, columnspan=3)
 
@@ -123,35 +147,41 @@ class Calculator:
             self.window,
             text="+",
             width=cfg.OPER_WIDTH,
+            height=cfg.OPER_HEIGHT,
             command=lambda: self.add_sym_to_input("+"),
         ).grid(row=2, column=3)
         tk.Button(
             self.window,
             text="-",
             width=cfg.OPER_WIDTH,
+            height=cfg.OPER_HEIGHT,
             command=lambda: self.add_sym_to_input("-"),
         ).grid(row=3, column=3)
         tk.Button(
             self.window,
             text="*",
             width=cfg.OPER_WIDTH,
+            height=cfg.OPER_HEIGHT,
             command=lambda: self.add_sym_to_input("*"),
         ).grid(row=4, column=3)
         tk.Button(
             self.window,
             text="/",
             width=cfg.OPER_WIDTH,
+            height=cfg.OPER_HEIGHT,
             command=lambda: self.add_sym_to_input("/"),
         ).grid(row=5, column=3)
         tk.Button(
             self.window,
             text="=",
             width=cfg.OPER_WIDTH * 2,
+            height=cfg.OPER_HEIGHT,
             command=self.calculate_and_display_result,
         ).grid(row=6, column=2, columnspan=2)
         tk.Button(
             self.window,
             text="Del",
             width=cfg.OPER_WIDTH * 2,
+            height=cfg.OPER_HEIGHT,
             command=self.clear_input_label,
         ).grid(row=6, column=0, columnspan=2)
--- 07_calculator/config.py
+++ 07_calculator/config.py
@@ -11,5 +11,12 @@ with open("./data/config.json", mode="r", encoding="UTF-8") as file:
 APP_TITLE = config_dict.get("app_title")
 APP_WIDTH = config_dict.get("app_width")
 APP_HEIGHT = config_dict.get("app_height")
+
+LABEL_WIDTH = config_dict.get("label_width")
+LABEL_HEIGHT = config_dict.get("label_height")
+
 NUM_WIDTH = config_dict.get("num_width")
+NUM_HEIGHT = config_dict.get("num_height")
+
 OPER_WIDTH = config_dict.get("oper_width")
+OPER_HEIGHT = config_dict.get("oper_height")
