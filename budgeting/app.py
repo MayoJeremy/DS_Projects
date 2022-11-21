5c51278f73065217d62f5a8a1099e0fb11e1f14d
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-21 14:07:00 +0100

    Add Wallet to DB Method

--- budgeting/app.py
+++ budgeting/app.py
@@ -1,6 +1,7 @@
 import management.config as cfg
 from management.dbman import Dbman
 from budgeter.user import User
+from budgeter.wallet import Wallet
 
 
 def is_Registered():
@@ -17,6 +18,8 @@ def main():
     else:
         print("User not in DB")
         #user = User.register_user_from_form("Test")
+    user_wallet_1 = Wallet.create_and_add_wallet_to_db(user.id, "Geldbeutel")
+    print(user_wallet_1)
     print(user_dbman)
     print(user)
 
--- budgeting/budgeter/wallet.py
+++ budgeting/budgeter/wallet.py
@@ -8,3 +8,17 @@ class Wallet:
         self.id = id
         self.user_id = user_id
         self.name = name
+
+    def __repr__(self):
+        return f"{self.name} from {self.user_id}"
+
+    @classmethod
+    def create_and_add_wallet_to_db(cls, user_id, name):
+        wallet_id = Wallet.save_wallet_to_db(user_id, name)
+        return Wallet(wallet_id, user_id, name)
+
+    @staticmethod
+    def save_wallet_to_db(user_id, name):
+        sql_statement = "INSERT INTO wallet (user_id,name) VALUES(%s,%s)"
+        return Wallet.dbman.insert_entry_and_retrieve_id(
+            sql_statement, (user_id, name))
