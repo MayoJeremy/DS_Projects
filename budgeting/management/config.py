7d9e5cd881325026f3e31e2a772662254bcea083
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-21 14:33:57 +0100

    income/expense categories

--- budgeting/management/config.py
+++ budgeting/management/config.py
@@ -9,9 +9,16 @@ MANAGEDIR = "./management"
 APPDIR = "./budgeter"
 
 CONFIG_FILE_NAME = "config.json"
+CATEGORY_FILE_NAME = "categories.json"
 
 
 with open(DATADIR+CONFIG_FILE_NAME, mode="r", encoding="UTF-8") as file_in:
     data_dict = json.load(file_in)
 
+with open(DATADIR+CATEGORY_FILE_NAME, mode="r", encoding="UTF-8") as file_in:
+    category_dict = json.load(file_in)
+
+
 DB_CRED = data_dict.get("db_cred")
+CAT_INCOME = category_dict.get("Income")
+CAT_EXPENSE = category_dict.get("Expense")
