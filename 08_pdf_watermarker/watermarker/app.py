55a925710f6cde0dcd023af57fe9cca24daac29b
MayoJeremy <Mayo.a.Jeremy@gmail.com>
2022-11-21 09:42:47 +0100

    Static Watermarking on Page and Footer

--- 08_pdf_watermarker/watermarker/app.py
+++ 08_pdf_watermarker/watermarker/app.py
@@ -1,7 +1,51 @@
 import config as cfg
+from PyPDF2 import PdfWriter, PdfReader
+from reportlab.pdfgen.canvas import Canvas
+
+
+def watermarker(base_pdf: str, result_pdf: str):
+    reader = PdfReader(cfg.DATADIR+base_pdf)
+    writer = PdfWriter()
+    page_indices = list(range(0, len(reader.pages)))
+    for index in page_indices:
+        content_page = reader.pages[index]
+        mediabox = content_page.mediabox
+
+        reader_foot_stamp = PdfReader(
+            cfg.DATADIR+cfg.FOOTER_TEMPLATE_FILE.get("temp_file_name"))
+        reader_stamp = PdfReader(
+            cfg.DATADIR+cfg.STAMP_TEMPLATE_FILE.get("temp_file_name"))
+        stamp_page = reader_stamp.pages[0]
+        image_page = reader_foot_stamp.pages[0]
+
+        image_page.merge_page(stamp_page)
+        image_page.merge_page(content_page)
+        image_page.mediabox = mediabox
+        writer.add_page(image_page)
+
+    with open(cfg.RESULTDIR+result_pdf, "wb") as fp:
+        writer.write(fp)
+
+
+def create_footer():
+    create_watermark_template(**cfg.FOOTER_TEMPLATE_FILE)
+
+
+def create_water_stamp():
+    create_watermark_template(**cfg.STAMP_TEMPLATE_FILE)
+
+
+def create_watermark_template(temp_file_name, x_cord, y_cord, deg_rotation=0):
+    canvas = Canvas(cfg.DATADIR + temp_file_name)
+    canvas.rotate(deg_rotation)
+    canvas.drawString(x=x_cord, y=y_cord, text=cfg.WATERTEXT)
+    canvas.save()
+
 
 def main():
-    pass
+    create_footer()
+    create_water_stamp()
+    watermarker("Watermark.pdf", "new_water.pdf")
 
 
 if __name__ == "__main__":
--- 08_pdf_watermarker/watermarker/config.py
+++ 08_pdf_watermarker/watermarker/config.py
@@ -5,8 +5,14 @@ from pathlib import Path
 os.chdir(Path(__file__).parent.parent)
 
 DATADIR = "./data/"
+RESULTDIR = DATADIR+"output/"
 CONFIG_FILE_NAME = "config.json"
 
 
 with open(DATADIR+CONFIG_FILE_NAME, mode="r", encoding="UTF-8") as file_in:
     data_dict = json.load(file_in)
+
+
+WATERTEXT = data_dict.get("watermark")
+FOOTER_TEMPLATE_FILE = data_dict.get("footer")
+STAMP_TEMPLATE_FILE = data_dict.get("waterstamp")
